977d07f2c93d462827e2c64cf72eb9874f51947f
Rogério do Carmo <rogerio.carmo02@gmail.com>
2019-08-26 12:16:45 -0300

    AVG Filter generalized to nxn image

--- CMD_interface.cpp
+++ CMD_interface.cpp
@@ -3,11 +3,19 @@
 #include "Filter.cpp"
 #include "Image.cpp"
 
+#define NEW_LINE_SINGLE printf("\n")
+#define NEW_LINE_DOUBLE printf("\n\n")
+#define HEADER_LINE_SINGLE  printf("\n============================================================\n")
+#define DIVISOR_LINE_SINGLE printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
 using namespace std;
 
 int main()
 {
     char * fileName = "C:/Users/Rogerio/Desktop/lenna.pgm";
+
+    //char * fileName = "C:/Users/Rogerio/Desktop/marcie.pgm";
+
     Image * img1 = new Image(fileName);
 
 
@@ -15,7 +23,7 @@ int main()
 
     img1->filter_median();
 
-    char * fileNameProcessed = "C:/Users/Rogerio/Desktop/meu_7.pgm";
+    char * fileNameProcessed = "C:/Users/Rogerio/Desktop/meu_77.pgm";
 
     img1->write_PGMfile(fileNameProcessed);
 
--- Image.cpp
+++ Image.cpp
@@ -11,7 +11,7 @@ class Image {
 
     private:
         int width, height;
-        double mPixels[256][256];
+        double ** mPixels;
         //string name;
 
     public:
@@ -40,6 +40,8 @@ class Image {
         char nmagico[3];
         int larg,alt,valmax;
 
+        //double matriz;
+
         read_PGMfile(char * fileName) {
             FILE *arq;
 
@@ -52,24 +54,34 @@ class Image {
             //strcpy(nome, fileName.c_str());
 
 
-            int i,j,w,z,aux,matriz[256][256],
-                    matriz2[256][256];
+            int i,j,w,z,aux;//,matriz[256][256],
+                    //matriz2[256][256];
                     //min=INT_MAX,max=INT_MIN;
 
             arq = fopen(fileName,"r");
             if (arq != NULL) {
             fscanf(arq,"%s",nmagico);
+
             int result = fscanf(arq,"%i %i",&larg,&alt);
+
+            //double matriz [larg][alt];
+
+            //if (result == 0) { // Line comment #
+//                fscanf(arq,"%i %i",&larg,&alt);
+  //          }
+
             fscanf(arq,"%i",&valmax);
 
             setWidth(larg);
             setHeight(alt);
 
+            mPixels = alocar(getHeight(), getWidth());
+
             //printf("%s %i %i %i\n",nmagico,larg,alt,valmax);
             // fclose(arq);
             // return 0;
-            for(i=0;i<256;i++) {
-                for(j=0;j<256;j++) {
+            for(i=0;i<getHeight();i++) {
+                for(j=0;j<getWidth();j++) {
                     fscanf(arq,"%i",&aux);
                     mPixels[i][j] = aux;
 
@@ -84,11 +96,22 @@ class Image {
             }
        }
 
+       double ** alocar(int l, int c) {
+            int i;
+            double **pm;
+            pm = (double **) calloc(l, sizeof(double *));
+
+            for (i = 0; i < l; i++) {
+                pm[i] = (double *) calloc(c,sizeof(double));
+            }
+            return (pm);
+        }
+
        filter_median() {
            int i, j, x, y;
 
            double soma;
-           int DIMM = 7;
+           int DIMM = 15;
            int DIMM_DIMM = DIMM * DIMM;
 
            int meio = DIMM / 2;
@@ -96,39 +119,46 @@ class Image {
            int pos_x;
            int pos_y;
 
-           for (i = 0; i < 256; i++) {
-                for (j = 0; j < 256; j++) {
+           cout << "Filter: " << getWidth() << " " << getHeight();
+
+          // double matriz[getWidth()][getHeight()];
+
+           for (i = 0; i < getHeight(); i++) {
+                for (j = 0; j < getWidth(); j++) {
                     soma = 0;
                     for (x = 0; x < DIMM; x++) {
                         //pos_x = i + x;
                         pos_x = i - meio + x;
-                        if (pos_x < 0 || pos_x > getHeight())
+                        if (pos_x < 0 || pos_x >= getHeight())
                             continue;
 
                         for (y = 0; y < DIMM; y++) {
                             //pos_y = j + y;
                             pos_y = j - meio + y;
-                            if (pos_y < 0 || pos_y > getWidth())
+                            if (pos_y < 0 || pos_y >= getWidth())
                                 continue;
 
                             //soma += mPixels[i + x][j + y];
                             soma += mPixels[pos_x][pos_y];
                         }
+                        //cout << "Laço y\n";
                     }
+                    //cout << "Atribuir ao pixel!\n";
                     mPixels[i][j] = ( ((double)soma / DIMM_DIMM) + 0.5);
                 } // end j for
+                //cout << "Fim de uma linha!\n";
            }// end i for
 
        }
 
        write_PGMfile(char * fileName) {
-             FILE * arq = fopen(fileName,"w");
+            FILE * arq = fopen(fileName,"w");
             if (arq != NULL) {
             // max = 255 - max;
             fprintf(arq,"%s\n%i %i\n%i\n",nmagico,larg,alt,valmax);
             int i, j;
-            for(i=0;i<256;i++) {
-                for(j=0;j<256;j++) {
+            for(i=0;i<getHeight();i++) {
+                for(j=0;j<getWidth();j++) {
                     fprintf(arq,"%i ", (int)mPixels[i][j]);
                 }
                 fprintf(arq,"\n");
