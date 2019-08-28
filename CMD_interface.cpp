360c02bc250d6c7b3566ab1b1f827319e20b7920
Rogério do Carmo <rogerio.carmo02@gmail.com>
2019-08-27 22:55:59 -0300

    All filters OK

--- CMD_interface.cpp
+++ CMD_interface.cpp
@@ -4,8 +4,8 @@
 
 #define NEW_LINE_SINGLE printf("\n")
 #define NEW_LINE_DOUBLE printf("\n\n")
-#define HEADER_LINE_SINGLE  printf("\n==============================================================================\n")
-#define DIVISOR_LINE_SINGLE printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+#define HEADER_LINE_SINGLE  printf("\n==================================================================================\n")
+#define DIVISOR_LINE_SINGLE printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
 using namespace std;
 
@@ -18,22 +18,54 @@ int main() {
     //char * fileName = "C:/Users/Rogerio/Desktop/processamentos/lenna.pgm";
     char * fileName = "C:/Users/Rogerio/Desktop/processamentos/marcie.pgm";
 
-    Image * img1 = new Image(fileName);
+    Image * imgOriginal = new Image(fileName);
 
-    cout << "\nImagem: " << fileName << " Dimensoes: "<< img1->getWidth()  << " x " << img1->getHeight() << endl << endl;
+    cout << "\nImagem: " << fileName << " || Dimensoes: "<< imgOriginal->getWidth () << " x " <<
+                                                            imgOriginal->getHeight() << endl  << endl;
 
-    Image * imgLaplace = img1->laplaciano();
-    Image * imgAverage = img1->filter_avg(9);
+    Image * imgLaplace1 = imgOriginal->laplaciano1();
+    Image * imgLaplace2 = imgOriginal->laplaciano2();
+    int dim_filter = 9;
+    Image * imgAverage  = imgOriginal->filter_avg(dim_filter);
 
-    imgLaplace->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Laplace.pgm");
-    imgAverage->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Average.pgm");
+    imgLaplace1->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Laplace_01.pgm");
+    imgLaplace2->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Laplace_02.pgm");
+    imgAverage ->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Average.pgm");
 
-    printf("Calculados os filtros da media e Laplaciano");
+    NEW_LINE_SINGLE;
+    printf("Calculados os filtros da Media e Laplaciano!");
+
+    // ===================================================================================
+    NEW_LINE_DOUBLE;
+    DIVISOR_LINE_SINGLE;
+    NEW_LINE_DOUBLE;
+    // ===================================================================================
+
+    Image * imgLight  = imgOriginal->img_lighten();
+    Image * imgDark   = imgOriginal->img_darken ();
+
+    imgLight->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Clara.pgm");
+    imgDark ->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Escura.pgm");
+
+    printf("Calculadas as imagens clareadas e escurecidas!");
+
+    // ===================================================================================
+    NEW_LINE_DOUBLE;
+    DIVISOR_LINE_SINGLE;
+    NEW_LINE_DOUBLE;
+    // ===================================================================================
+
+    Image * sumOriLapl = imgOriginal->sumImage(imgLaplace1);
+    sumOriLapl->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Soma_ORI_LAPL.pgm");
+
+    Image * sumAVGLapl = imgAverage ->sumImage(imgLaplace1);
+    sumAVGLapl->write_PGMfile("C:/Users/Rogerio/Desktop/processamentos/Proc_Soma_AVG_LAPL.pgm");
+
+    printf("Calculadas as somas das imagens!");
     NEW_LINE_DOUBLE;
 
-    DIVISOR_LINE_SINGLE;
-
-    printf("Pressione qualquer tecla para continuar... ");
+    printf("Pressione qualquer tecla para finalizar... ");
     getchar();
+
     return 0;
 }
--- Image.cpp
+++ Image.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string.h>
 #include <float.h>
-#include <string>
 
 #define MAX_PIXEL_VALUE 255
 #define MAGICAL_NUMBER_ASCII_PGM "P2"
@@ -14,13 +13,18 @@ using namespace std;
 class Image {
 
     private:
+
         int width, height;
         int maxValue; int minValue;
         float ** mPixels;
-        //float ** mPixels2;
+
     public:
 
         Image() {
+        }
+
+        ~Image() {
+            liberar(getHeight(),mPixels);
         }
 
         Image(int height, int width) {
@@ -57,46 +61,56 @@ class Image {
             return ( mPixels[x][y] );
         }
 
-        //double matriz;
 
         read_PGMfile(char * fileName) {
             FILE *arq;
 
-            //cout << "Nome: " << fileName << " \n";
             char nmagico[3];
             int larg,alt,valmax;
 
-            int i,j,w,z,aux;//,matriz[256][256],
-                    //matriz2[256][256];
-                    //min=INT_MAX,max=INT_MIN;
-
-            maxValue = FLT_MAX; minValue = FLT_MIN;
+            int i,j,w,z,aux;
+            maxValue = FLT_MIN; minValue = FLT_MAX;
 
             arq = fopen(fileName,"r");
             if (arq != NULL) {
-            fscanf(arq,"%s",nmagico);
-            fscanf(arq,"%i %i",&larg,&alt);
-            fscanf(arq,"%i",&valmax);
+                fscanf(arq,"%s",nmagico);
+                fscanf(arq,"%i %i",&larg,&alt);
+                fscanf(arq,"%i",&valmax);
 
-            setWidth(larg);
-            setHeight(alt);
+                setWidth(larg);
+                setHeight(alt);
 
-            mPixels = alocar(getHeight(), getWidth());
+                mPixels = alocar(getHeight(), getWidth());
 
-            //printf("%s %i %i %i\n",nmagico,larg,alt,valmax);
-            // fclose(arq);
-            // return 0;
-            for(i=0;i<getHeight();i++) {
-                for(j=0;j<getWidth();j++) {
-                    fscanf(arq,"%i",&aux);
-                    mPixels[i][j] = aux;
+                for(i = 0;i < getHeight(); i++) {
+                    for(j = 0;j < getWidth(); j++) {
+                        fscanf(arq,"%i",&aux);
+                        mPixels[i][j] = aux;
 
-                 if(aux < minValue) {minValue=aux;}
-                 if(aux > maxValue) {minValue=aux;}
+                    if(aux < minValue) {minValue = aux;}
+                    if(aux > maxValue) {maxValue = aux;}
+                    }
                 }
+                fclose(arq);
             }
-            fclose(arq);
-            }
+       }
+
+       write_PGMfile(char * fileName) {
+            FILE * arq = fopen(fileName,"w");
+            if (arq != NULL) {
+                fprintf(arq,"%s\n%i %i\n%i\n",MAGICAL_NUMBER_ASCII_PGM,getWidth(),getHeight(),MAX_PIXEL_VALUE);
+                int i, j;
+                for(i = 0; i < getHeight(); i++) {
+                    for(j = 0; j < getWidth(); j++) {
+                        fprintf(arq,"%i ", getPixelAsInt(i,j));
+                    }
+                    fprintf(arq,"\n");
+                }
+                fclose(arq);
+                }
+                else {
+                    printf("Erro no arquivo de saida\n");
+                }
        }
 
        float ** alocar(int l, int c) {
@@ -118,21 +132,45 @@ class Image {
             free(pm);
         }
 
-       Image * laplaciano(){
+       Image * laplaciano1(){
            Image * newImage = new Image(getHeight(),getWidth());
 
             int i, j, z, w;
             float aux;
             int H[3][3];
 
-            //mPixels2 = alocar(getHeight(),getWidth());
-
             H[0][0] =  0; H[0][1] = -1; H[0][2] =  0;
             H[1][0] = -1; H[1][1] =  4; H[1][2] = -1;
             H[2][0] =  0; H[2][1] = -1; H[2][2] =  0;
 
-            for(i=1; i < (getHeight() - 1); i++) {
-                for(j=1; j < (getWidth() -1); j++) {
+            for(i = 1; i < (getHeight() - 1); i++) {
+                for(j = 1; j < (getWidth() -1); j++) {
+                    aux = 0;
+                    for (w = -1; w <= 1; w++) {
+                        for (z = -1; z <= 1; z++) {
+                            aux += getPixelAsFloat(i+w, j+z) * H[w + 1][z + 1];
+                        }
+                    }
+
+                    newImage->setPixel(i, j, ((aux) + 0.5));
+                }
+            }
+        return newImage;
+    }
+
+    Image * laplaciano2(){
+           Image * newImage = new Image(getHeight(),getWidth());
+
+            int i, j, z, w;
+            float aux;
+            int H[3][3];
+
+            H[0][0] = -1; H[0][1] = -1; H[0][2] = -1;
+            H[1][0] = -1; H[1][1] =  8; H[1][2] = -1;
+            H[2][0] = -1; H[2][1] = -1; H[2][2] = -1;
+
+            for(i = 1; i < (getHeight() - 1); i++) {
+                for(j = 1; j < (getWidth() -1); j++) {
                     aux=0;
                     for (w = -1; w <= 1; w++) {
                         for (z = -1; z <= 1; z++) {
@@ -146,6 +184,31 @@ class Image {
         return newImage;
     }
 
+    Image * img_lighten() {
+        Image * newImage = new Image(getHeight(),getWidth());
+
+        int i, j;
+        for (i = 0; i < getHeight(); i++) {
+                for (j = 0; j < getWidth(); j++) {
+                    newImage->setPixel(i,j, MAX_PIXEL_VALUE - maxValue + getPixelAsFloat(i,j));
+                }
+           }
+
+        return newImage;
+    }
+
+    Image * img_darken() {
+        Image * newImage = new Image(getHeight(),getWidth());
+
+        int i, j;
+        for (i = 0; i < getHeight(); i++) {
+                for (j = 0; j < getWidth(); j++) {
+                    newImage->setPixel(i,j, getPixelAsFloat(i,j) - minValue);
+                }
+           }
+
+        return newImage;
+    }
 
     Image * filter_avg(int dim) {
         Image * newImage = new Image(getHeight(),getWidth());
@@ -183,22 +246,21 @@ class Image {
             return newImage;
        }
 
-       write_PGMfile(char * fileName) {
-            FILE * arq = fopen(fileName,"w");
-            if (arq != NULL) {
-            // max = 255 - max;
-            fprintf(arq,"%s\n%i %i\n%i\n",MAGICAL_NUMBER_ASCII_PGM,getWidth(),getHeight(),MAX_PIXEL_VALUE);
-            int i, j;
-            for(i=0;i<getHeight();i++) {
-                for(j=0;j<getWidth();j++) {
-                    fprintf(arq,"%i ", getPixelAsInt(i,j));
+       Image * sumImage(Image * anotherImage) {
+           if (anotherImage->getHeight() != getHeight() || anotherImage->getWidth() != getWidth()) return NULL;
+
+           Image * newImage = new Image(getHeight(),getWidth());
+
+           int i, j, aux;
+
+           for (i = 0; i < getHeight(); i++) {
+                for (j = 0; j < getWidth(); j++) {
+                    aux = getPixelAsFloat(i,j) + anotherImage->getPixelAsFloat(i,j);
+                    if (aux > 255) aux = 255;
+                    if (aux <  0 ) aux = 0;
+                    newImage->setPixel(i,j, aux);
                 }
-                fprintf(arq,"\n");
-            }
-            fclose(arq);
-            }
-            else {
-                printf("Erro no arquivo de saida\n");
-            }
+           }
+           return newImage;
        }
 };
